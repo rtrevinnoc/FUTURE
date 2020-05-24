cf08db74e7d93a1b40175be0e7950c5571575230
roberto <rtrevinnoc@hotmail.com>
2020-05-23 22:26:17 -0500

    Spider is now crawling WSJ

--- indexer.py
+++ indexer.py
@@ -144,7 +144,7 @@ class Indexer(scrapy.Spider):
         "AJAXCRAWL_ENABLED": True
     }
 
-    start_urls = ["https://techcrunch.com/"]
+    start_urls = ["https://www.wsj.com/"]
 
     def parse(self, response) -> Iterator:
         webPageVector = getWebpageMeanVector(response)
