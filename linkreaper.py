4bfa48dcbbf98f5a578dfea0faaff5e1c02e05df
rtrevinnoc <rtrevinnoc@hotmail.com>
2020-06-17 01:07:05 -0500

    Crawler now starts at WSJ

--- linkreaper.py
+++ linkreaper.py
@@ -138,7 +138,7 @@ class Indexer(scrapy.Spider):
         "AJAXCRAWL_ENABLED": True
     }
 
-    start_urls = ["https://www.wired.com/"]
+    start_urls = ["https://www.wsj.com/"]
 
     def parse(self, response) -> Iterator:
         url = response.request.url
