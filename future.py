f09446065a29aa54334f5b5af87857b5613cfcf0
rtrevinnoc <rtrevinnoc@wearebuildingthefuture.com>
2020-10-22 17:14:35 -0500

    TESTING: peer network

--- future.py
+++ future.py
@@ -160,22 +160,20 @@ def sendAnswerRequestToPeer(url, query, queryVector, queryLanguage):
     print("#######################")
     if peer == hostIP or peer == hostname:
         print("Same as origin")
-        return {}
     else:
         try:
             r = requests.get("http://" + peer + "/_answerPeer", params={'query': query, 'q_vec': queryVector, 'queryLanguage': queryLanguage}, timeout=15)
             result = r.json()["result"]
             print("Obtained with http")
-            return {"urls": list(zip(result["urls"], result["url_scores"])), "images": list(zip(result["images"], result["images_scores"]))}
+            return {"urls": zip(result["urls"], result["url_scores"]), "images": zip(result["images"], result["images_scores"])}
         except:
             try:
                 r = requests.get("https://" + peer + "/_answerPeer", params={'query': query, 'q_vec': queryVector, 'queryLanguage': queryLanguage}, timeout=15)
                 result = r.json()["result"]
                 print("Obtained with https")
-                return {"urls": list(zip(result["urls"], result["url_scores"])), "images": list(zip(result["images"], result["images_scores"]))}
+                return {"urls": zip(result["urls"], result["url_scores"]), "images": zip(result["images"], result["images_scores"])}
             except:
                 print("Could not connect with peer")
-                return {}
 
 with peerRegistry.begin() as peerRegistryDBTransaction:
     peerRegistryDBSelector = peerRegistryDBTransaction.cursor()
