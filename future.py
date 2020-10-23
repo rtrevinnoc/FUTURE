22a3661aba1cdb2c1a298205be7e83681e3eb39a
rtrevinnoc <rtrevinnoc@wearebuildingthefuture.com>
2020-10-23 18:08:32 -0500

    TESTING: peer network

--- future.py
+++ future.py
@@ -165,12 +165,20 @@ def sendAnswerRequestToPeer(url, query, queryVector, queryLanguage):
             r = requests.get("http://" + peer + "/_answerPeer", params={'query': query, 'q_vec': queryVector, 'queryLanguage': queryLanguage}, timeout=200)
             result = r.json()["result"]
             print("Obtained with http")
+            try:
+                print(result["urls"])
+            except:
+                pass
             return {"urls": list(zip(result["urls"], result["url_scores"])), "images": list(zip(result["images"], result["images_scores"]))}
         except:
             try:
                 r = requests.get("https://" + peer + "/_answerPeer", params={'query': query, 'q_vec': queryVector, 'queryLanguage': queryLanguage}, timeout=200)
                 result = r.json()["result"]
                 print("Obtained with https")
+                try:
+                    print(result["urls"])
+                except:
+                    pass
                 return {"urls": list(zip(result["urls"], result["url_scores"])), "images": list(zip(result["images"], result["images_scores"]))}
             except:
                 print("Could not connect with peer")
