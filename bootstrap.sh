9ab30d9f2f1d310343b9a565f0e74de992877d86
rtrevinnoc <rtrevinnoc@wearebuildingthefuture.com>
2020-11-03 07:40:37 -0600

    Minor corrections to install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,10 +13,13 @@ else
    pip install -U torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 fi
 
-sudo apt-get install libssl-dev python3-icu
+sudo apt-get install libssl-dev python3-icu libicu-dev
 sudo zypper install gcc-c++ openssl-devel
 sudo pacman -S python-pyopenssl
 
+pip3 install -U pybind11
+pip3 install -U PyICU
+pip3 install -U pycld2
 pip3 install -U flask
 pip3 install -U SPARQLWrapper
 pip3 install -U gensim
@@ -38,8 +41,6 @@ pip3 install -U symspellpy
 pip3 install -U polyglot
 pip3 install -U flask_wtf
 pip3 install -U polyglot
-pip3 install -U PyICU
-pip3 install -U pycld2
 pip3 install -U werkzeug==0.16.1
 pip3 install -U scrapy
 pip3 install -U tldextract
@@ -52,10 +53,12 @@ python3 -m spacy download en_core_web_sm
 python3 -c "import nltk; nltk.download('punkt')"
 
 chmod +x future.py
+chmod +x save_index.sh
+chmod +x build_index.sh
 
-echo "All dependencies installed"
-echo "Terminate now with CTRL+C if you do not want to train any machine learning model"
-read -p "Otherwise, press enter to continue"
+echo "All dependencies installed. You can now proceed to build the index with <<build_index.sh>> and later pause the process with <<save_index.sh>>. Run future with <<python3 future.py>>"
+# echo "Terminate now with CTRL+C if you do not want to train any machine learning model"
+# read -p "Otherwise, press enter to continue"
 
 ################################################################### TRAIN THE MODELS #####
 
@@ -64,15 +67,15 @@ read -p "Otherwise, press enter to continue"
 
 ################################################################### TRAIN THE MODELS #####
 
-echo "Building index..."
-echo "Terminate at any time with CTRL+C and proceed to execute ./save_index.sh"
-echo "The process can be resumed later by running ./build_index.sh again"
-chmod +x save_index.sh
-chmod +x build_index.sh
-./build_index.sh
+# echo "Building index..."
+# echo "Terminate at any time with CTRL+C and proceed to execute ./save_index.sh"
+# echo "The process can be resumed later by running ./build_index.sh again"
+# chmod +x save_index.sh
+# chmod +x build_index.sh
+# ./build_index.sh
 
 ##########################################################################################
 
-echo "Proceed to execute ./save_index.sh"
-echo "And finally start the server with ./future.py"
-echo "The process of building the index can be resumed later by running ./build_index.sh again"
+# echo "Proceed to execute ./save_index.sh"
+# echo "And finally start the server with ./future.py"
+# echo "The process of building the index can be resumed later by running ./build_index.sh again"
